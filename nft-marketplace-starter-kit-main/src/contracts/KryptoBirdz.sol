d4db7c9ad6fbff02d842b4e0dff2a357c30af32b
Zajason <zakjason04@gmail.com>
2022-05-02 21:18:18 +0300

    hello

---
+++ nft-marketplace-starter-kit-main/src/contracts/KryptoBirdz.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract KryptoBird {
+    string public name;
+    string public symbol;
+
+
+    constructor(){
+        name = 'KryptoBird';
+        symbol = 'KBIRDZ';
+
+    }
+}
